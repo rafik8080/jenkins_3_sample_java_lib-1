c393a3e8a50b734cd8399c915418983c011980a5
Effective Jenkins <effectivejenkins@gmail.com>
2018-04-08 17:36:18 +0200

    Fix msg

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -4,7 +4,7 @@
 public class Library {
     public boolean someLibraryMethod() {
         System.out.println("Hello World!");
-        System.out.println("Testing PR builds!");
+        System.out.println("Testing PR builds!!!");
         return true;
     }
 }
